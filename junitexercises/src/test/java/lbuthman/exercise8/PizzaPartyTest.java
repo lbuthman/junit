3c4615089e6f2e45d831fa88e88237ca51094f7f
Luke Buthman <lbuthman@gmail.com>
2017-11-21 20:27:44 -0600

    add test case for exercise 8

--- junitexercises/src/test/java/lbuthman/exercise8/PizzaPartyTest.java
+++ junitexercises/src/test/java/lbuthman/exercise8/PizzaPartyTest.java
@@ -1,4 +1,14 @@
 package lbuthman.exercise8;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class PizzaPartyTest {
+
+    @Test
+    public void testCalculateTotalSlices() {
+        PizzaCalculator pizzaCalculator = new PizzaCalculator();
+        double totalSlices = pizzaCalculator.calculateTotalSlices(2);
+        assertEquals(16, totalSlices, 0);
+    }
 }
--- junitexercises/src/test/java/lbuthman/suites/Ch3TestSuite.java
+++ junitexercises/src/test/java/lbuthman/suites/Ch3TestSuite.java
@@ -1,4 +1,11 @@
 package lbuthman.suites;
 
+import lbuthman.exercise7.SquareTest;
+import lbuthman.exercise8.PizzaPartyTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value=Suite.class)
+@Suite.SuiteClasses(value={SquareTest.class, PizzaPartyTest.class})
 public class Ch3TestSuite {
 }
