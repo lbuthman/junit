85a1ca5d335d5510f9e26463def14ea898591595
Luke Buthman <lbuthman@gmail.com>
2017-11-23 07:25:06 -0600

    add tests for exercise14

--- junitexercises/src/test/java/lbuthman/exercise14/TaxCalculatorTest.java
+++ junitexercises/src/test/java/lbuthman/exercise14/TaxCalculatorTest.java
@@ -1,4 +1,25 @@
 package lbuthman.exercise14;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TaxCalculatorTest {
+
+    @Test
+    public void testWisconsinTax() {
+        TaxCalculator taxCalculator = new TaxCalculator();
+        taxCalculator.setSubtotal(10.00);
+        taxCalculator.setState("WI");
+        double tax = taxCalculator.calculateTax();
+        assertEquals(0.55, tax, 0);
+    }
+
+    @Test
+    public void testGenericTax() {
+        TaxCalculator taxCalculator = new TaxCalculator();
+        taxCalculator.setSubtotal(10.00);
+        taxCalculator.setState("OK");
+        double tax = taxCalculator.calculateTax();
+        assertEquals(0, tax, 0);
+    }
 }
--- junitexercises/src/test/java/lbuthman/suites/Ch4TestSuite.java
+++ junitexercises/src/test/java/lbuthman/suites/Ch4TestSuite.java
@@ -1,4 +1,10 @@
 package lbuthman.suites;
 
+import lbuthman.exercise14.TaxCalculatorTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(value={TaxCalculatorTest.class})
 public class Ch4TestSuite {
 }
