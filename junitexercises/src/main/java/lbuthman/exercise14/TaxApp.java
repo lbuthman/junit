55f3201ad14b759d9cad00bd2110faaa755e168b
Luke Buthman <lbuthman@gmail.com>
2017-11-23 07:25:20 -0600

    refactor solution for easier testing

--- junitexercises/src/main/java/lbuthman/exercise14/TaxApp.java
+++ junitexercises/src/main/java/lbuthman/exercise14/TaxApp.java
@@ -5,25 +5,55 @@ import static tools.Tools.*;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class TaxCalculator {
+public class TaxApp {
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("0.00");
         Scanner input = getScanner();
+        TaxCalculator taxCalculator = new TaxCalculator();
 
         print("What is the order amount? ");
         double amount = input.nextDouble();
+        taxCalculator.setSubtotal(amount);
 
         print("What is the state? ");
         String state = input.next();
+        taxCalculator.setState(state);
 
-        double subtotal = amount;
+        double tax = taxCalculator.calculateTax();
+
+        println("The tax is $" + df.format(tax));
+        println("The total is $" + df.format(taxCalculator.getSubtotal() + tax));
+    }
+}
+
+class TaxCalculator {
+
+    private double subtotal;
+    private String state;
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    double calculateTax() {
         double tax = 0;
-        if (state.contains("WI")) {
+        if (getState().equals("WI")) {
             tax = 5.5 / 100;
-            tax *= subtotal;
-            println("The tax is $" + df.format(tax));
+            tax *= getSubtotal();
         }
 
-        println("The total is $" + df.format(subtotal + tax));
+        return tax;
     }
 }
