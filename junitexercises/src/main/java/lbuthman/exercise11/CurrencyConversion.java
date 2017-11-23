528d9a2a9d34eaddd3901cb9d23ae51b2c0aa901
Luke Buthman <lbuthman@gmail.com>
2017-11-22 20:55:27 -0600

    refactor solution for easier testing

--- junitexercises/src/main/java/lbuthman/exercise11/CurrencyConversion.java
+++ junitexercises/src/main/java/lbuthman/exercise11/CurrencyConversion.java
@@ -8,16 +8,25 @@ import java.util.Scanner;
 public class CurrencyConversion {
     public static void main(String[] args) {
 
-        double EXCHANGE_RATE = 1.18;
-
+        DecimalFormat df = new DecimalFormat("0.00");
         Scanner input = getScanner();
+
+        Converter converter = new Converter();
 
         print("How many euros are you exchanging? ");
         double euros = input.nextDouble();
 
-        DecimalFormat df = new DecimalFormat("0.00");
-        double usd = euros * EXCHANGE_RATE;
+        double usd = converter.convertEuroToUSD(euros);
 
-        print(euros + " euros at an exchange rate of " + EXCHANGE_RATE + " is " + "$" + df.format(usd));
+        print(euros + " euros at an exchange rate of " + converter.EXCHANGE_RATE + " is " + "$" + df.format(usd));
+    }
+}
+
+class Converter {
+
+    double EXCHANGE_RATE = 1.18143679254;
+
+    double convertEuroToUSD(double euros) {
+        return euros * EXCHANGE_RATE;
     }
 }
