3454aede580c005a8f73fd9ce9de7d6993fdb1cd
Luke Buthman <lbuthman@gmail.com>
2017-11-24 08:09:43 -0600

    refactor for easier testing

--- junitexercises/src/main/java/lbuthman/exercise19/BMIApp.java
+++ junitexercises/src/main/java/lbuthman/exercise19/BMIApp.java
@@ -5,37 +5,81 @@ import java.util.Scanner;
 
 import static tools.Tools.*;
 
-public class BMICalculator {
+public class BMIApp {
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("0.0");
         Scanner input = getScanner();
+        BMICalculator bmiCalculator = new BMICalculator();
 
         println("Let's get your height in feet and inches.");
         print("First, enter feet: ");
         double feet = input.nextDouble();
+        bmiCalculator.setFeet(feet);
 
         print("Next, enter inches: ");
         double inches = input.nextDouble();
-
-        double heightInInches = feet * 12 + inches;
+        bmiCalculator.setInches(inches);
 
         print("Finally, how much do you weight? ");
         double weight = input.nextDouble();
+        bmiCalculator.setWeight(weight);
 
-        double bmi = (weight / ( Math.pow(heightInInches, 2)) * 703);
+        double bmi = bmiCalculator.calculateBmi();
+        String result = bmiCalculator.getBmiResult(bmi);
 
         println("Your BMI is " + df.format(bmi));
-        if (bmi < 18.5) {
-            println("You are underweight.");
-        }
-        else if (bmi > 25) {
-            println("You are overweight.");
-        }
-        else {
-            println("You are within the ideal weight range.");
-            return;
+        println("You are " + result + ".");
+    }
+}
+
+class BMICalculator {
+
+    final private double NORMAL_LOW = 18.5;
+    final private double NORMAL_HIGH = 25;
+
+    private double feet;
+    private double inches;
+    private double weight;
+
+    public double getFeet() {
+        return feet;
+    }
+
+    public void setFeet(double feet) {
+        this.feet = feet;
+    }
+
+    public double getInches() {
+        return inches;
+    }
+
+    public void setInches(double inches) {
+        this.inches = inches;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    double calculateBmi() {
+        double heightInInches = feet * 12 + inches;
+
+        return (weight / ( Math.pow(heightInInches, 2)) * 703);
+    }
+
+    String getBmiResult(double bmi) {
+        if (bmi < NORMAL_LOW) {
+            return "Underweight\nYou should see your doctor.";
         }
 
-        println("You should see your doctor.");
+        if (bmi > NORMAL_HIGH) {
+            return "Overweight\nYou should see your doctor.";
+        }
+
+        return "Normal";
     }
 }
