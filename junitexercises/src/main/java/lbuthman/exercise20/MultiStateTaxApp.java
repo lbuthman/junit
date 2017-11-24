2f0515783883461d4301ca6e9bbe31250b2184d7
Luke Buthman <lbuthman@gmail.com>
2017-11-24 15:43:41 -0600

    refactor solution for easier testing

--- junitexercises/src/main/java/lbuthman/exercise20/MultiStateTaxApp.java
+++ junitexercises/src/main/java/lbuthman/exercise20/MultiStateTaxApp.java
@@ -5,18 +5,21 @@ import static tools.Tools.*;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class MultiStateTaxCalculator {
+public class MultiStateTaxApp {
     public static void main(String[] args) {
         final double ILLINOIS_TAX_RATE = 0.08;
 
         DecimalFormat df = new DecimalFormat("0.00");
         Scanner input = getScanner();
+        MultiStateTaxCalculator taxCalculator = new MultiStateTaxCalculator();
 
         print("What is the order amount? ");
         double amount = input.nextDouble();
+        taxCalculator.setAmount(amount);
 
         print("What state do you live in? ");
         String state = input.next();
+        taxCalculator.setState(state);
 
         double tax = 0.0;
         if (state.toLowerCase().equals("wisconsin")) {
@@ -24,12 +27,12 @@ public class MultiStateTaxCalculator {
             input.skip("\\s");
             String county = input.nextLine();
             input.reset();
-            double taxRate = getCountyTaxRate(county);
-            tax = calculateTax(amount, taxRate);
+            double taxRate = taxCalculator.getCountyTaxRate(county);
+            tax = taxCalculator.calculateTax(taxRate);
         }
 
         if (state.toLowerCase().equals("illinois")) {
-            tax = calculateTax(amount, ILLINOIS_TAX_RATE);
+            tax = taxCalculator.calculateTax(ILLINOIS_TAX_RATE);
         }
 
         double total = amount;
@@ -40,12 +43,34 @@ public class MultiStateTaxCalculator {
 
         println("The total is $" + df.format(total));
     }
+}
 
-    private static double calculateTax(double amount, double taxRate) {
+class MultiStateTaxCalculator {
+
+    private double amount;
+    private String state;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    double calculateTax(double taxRate) {
         return amount * taxRate;
     }
 
-    private static double getCountyTaxRate(String county) {
+    double getCountyTaxRate(String county) {
         final double EAU_CLAIRE_TAX_RATE = 0.05;
         final double DUNN_TAX_RATE = 0.04;
 
