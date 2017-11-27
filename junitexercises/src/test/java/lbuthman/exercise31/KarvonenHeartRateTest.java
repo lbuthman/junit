df62d4f2e883ceaa3a87619394816309e0b8a640
Luke Buthman <lbuthman@gmail.com>
2017-11-27 12:28:21 -0600

    add tests for exercise31

--- junitexercises/src/test/java/lbuthman/exercise31/KarvonenHeartRateTest.java
+++ junitexercises/src/test/java/lbuthman/exercise31/KarvonenHeartRateTest.java
@@ -1,4 +1,25 @@
 package lbuthman.exercise31;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class KarvonenHeartRateTest {
+
+    private int RESTING = 65;
+    private int AGE = 22;
+
+    @Test
+    public void testCalculateTargetHR55() {
+        double intensity = 55 / 100.0;
+        int result = KarvonenHeartRate.calculateTargetHR(AGE, RESTING, intensity);
+        assertEquals(138, result, 0);
+    }
+
+    @Test
+    public void testCalculateTargetHR95() {
+        double intensity = 95 / 100.0;
+        int result = KarvonenHeartRate.calculateTargetHR(AGE, RESTING, intensity);
+        assertEquals(191, result, 0);
+    }
 }
--- junitexercises/src/test/java/lbuthman/suites/Ch6TestSuite.java
+++ junitexercises/src/test/java/lbuthman/suites/Ch6TestSuite.java
@@ -1,4 +1,10 @@
 package lbuthman.suites;
 
+import lbuthman.exercise31.KarvonenHeartRateTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(value={KarvonenHeartRateTest.class})
 public class Ch6TestSuite {
 }
